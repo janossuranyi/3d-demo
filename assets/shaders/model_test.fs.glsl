eacd2a91d8c4c02638baa12191fa4baa2099ce48
suranyi.janos <suranyi.janos@eszfk.hu>
2022-09-01 12:35:18 +0200

    work

--- assets/shaders/model_test.fs.glsl
+++ assets/shaders/model_test.fs.glsl
@@ -1,5 +1,9 @@
 #version 450 core
 
+// PBR Specular-Glossiness model
+
+const float PI = 3.14159265359;
+
 out vec4 FragColor;
 
 in VS_OUT {
@@ -28,41 +32,113 @@ uniform sampler2D samp2_pbr;
 uniform sampler2D samp3_emissive;
 uniform sampler2D samp4_ao;
 
-vec4 fn_color1()
+vec3 fresnelSchlick(float cosTheta, vec3 F0)
 {
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+}  
 
-    vec3 normal = texture(samp1_normal, fs_in.TexCoords).rgb;
-    vec3 pbr    = texture(samp2_pbr, fs_in.TexCoords).rgb;
-    // transform normal vector to range [-1,1]
-    normal = normalize(normal * 2.0 - 1.0);  // this normal is in tangent space
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a      = roughness*roughness;
+    float a2     = a*a;
+    float NdotH  = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH*NdotH;
+	
+    float num   = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+	
+    return num / denom;
+}
 
-	vec3 lightColor = vec3(1.0, 1.0, 1.0);
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r*r) / 8.0;
 
-    vec3 color = pbr + texture(samp0_albedo, fs_in.TexCoords).rgb;
+    float num   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+	
+    return num / denom;
+}
 
-    // ambient
-    vec3 ambient = 0.1 * color;
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2  = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1  = GeometrySchlickGGX(NdotL, roughness);
+	
+    return ggx1 * ggx2;
+}
 
-    // diffuse
-    vec3 lightDir = normalize(fs_in.TangentLightPos - fs_in.TangentFragPos);
-    float diff = max(dot(lightDir, normal), 0.0);
-    vec3 diffuse = lightColor * diff * color;
 
-    // specular
-    vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
-    vec3 halfwayDir = normalize(lightDir + viewDir);  
-    float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
+vec3 light_radiance(float aDistance, float aRadius, vec3 aColor)
+{
+    float vD2 = aDistance * aDistance;
+    float vR2 = aRadius * aRadius;
+    float attenuation = 1.0 / (1.0 + 2 * aDistance / aRadius + vD2 / vR2);
 
-    vec3 specular = lightColor * pbr * spec;
+    return aColor * attenuation;
+}
 
-    return vec4(ambient + diffuse + specular, 1.0);
- 
+const float kLightRadius = 5;
+const vec3 kLightColor = vec3(1.0,1.0,1.0) * 10;
+
+vec3 toLinearRGB(vec3 sRGB)
+{
+    return pow(sRGB, vec3(2.2));
 }
 
 void main()
 {
 
-    FragColor = fn_color1();
+    vec3 albedo = toLinearRGB(texture(samp0_albedo, fs_in.TexCoords).rgb);
+    vec4 specularGlossiness = texture(samp2_pbr, fs_in.TexCoords);
+    vec3 spceularColor = toLinearRGB(specularGlossiness.rgb);
+
+    vec3 N = texture(samp1_normal, fs_in.TexCoords).rgb;
+    // transform normal vector to range [-1,1]
+    N = normalize(N * 2.0 - 1.0);  // this normal is in tangent space
+
+    // View direction vector
+    vec3 V = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
+    // Light direction vector
+    vec3 L = fs_in.TangentLightPos - fs_in.TangentFragPos;
+    float distance = length(L);
+    L /= distance;
+
+    // "Half" vector
+    vec3 H = normalize(V + L);
+
+    vec3 radiance = light_radiance(distance, kLightRadius, kLightColor);
+    vec3 diffColor = mix(spceularColor, albedo, step(vec3(0.0001), albedo));
+
+    vec3 F0 = spceularColor;
+    float roughness = 1-specularGlossiness.a;
+
+    // cook-torrance brdf
+    float NDF = DistributionGGX(N, H, roughness);        
+    float G   = GeometrySmith(N, V, L, roughness);      
+    vec3 F    = fresnelSchlick(max(dot(H, V), 0.0), F0);       
+    
+    vec3 kS = F;
+    vec3 kD = vec3(1.0) - kS;
+    //kD *= 1-max(max(spceularColor.r, spceularColor.g), spceularColor.b);
+        
+    vec3 numerator    = NDF * G * F;
+    float NdotL       = max(dot(N, L), 0.0);                
+    float denominator = 4.0 * max(dot(N, V), 0.0) * NdotL + 0.0001;
+    vec3 specular     = numerator / denominator;  
+            
+    // add to outgoing radiance Lo
+
+
+    vec3 color = (kD * diffColor / PI + specular) * radiance * NdotL;
+    color = color / (color + vec3(1.0));
+    color = pow(color, vec3(1/2.2));
+
+    FragColor = vec4(color,1);
 
 }
 
--- demo/demo.cpp
+++ demo/demo.cpp
@@ -247,7 +247,7 @@ int main(int argc, char** argv)
 
     Info("V_Init Start");
 
-    if (V_Init(SCREEN_WIDTH, SCREEN_HEIGHT, 0, FULLSCREEN))
+    if (V_Init(SCREEN_WIDTH, SCREEN_HEIGHT, 8, FULLSCREEN))
     {
         Info("V_Init Done");
 
