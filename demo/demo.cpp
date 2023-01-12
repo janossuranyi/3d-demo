063a0819d868518fa984492bb72bf2dfa5780c6c
suranyi.janos <suranyi.janos@eszfk.hu>
2023-01-12 17:05:21 +0100

    work

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -31,7 +31,7 @@ void App_EventLoop()
     bool running = true;
     float prev = float(SDL_GetTicks());
 
-    std::unique_ptr<Effect> activeEffect = std::make_unique<EngineTestEffect>();
+    std::unique_ptr<Effect> activeEffect = std::make_unique<BumpEffect>();
 
     if (!activeEffect->Init())
         return;
