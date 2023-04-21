97968fc1c059f29394d60ad37a6ab74a4fb6fb5b
Janos Suranyi <janos.suranyi@gmail.com>
2023-04-22 00:19:40 +0200

    work

--- engine2/Material.h
+++ engine2/Material.h
@@ -1,7 +1,16 @@
 #pragma once
+#include <string>
+#include <glm/glm.hpp>
+#include "./Image.h"
 
 namespace jsr {
 
+	enum eCoverage
+	{
+		COVERAGE_SOLID,
+		COVERAGE_MASK,
+		COVERAGE_BLEND
+	};
 
 	struct stage_t
 	{
@@ -9,6 +18,22 @@ namespace jsr {
 	};
 	class Material
 	{
-
+	public:
+		int id;
+		std::string name;
+		glm::vec4 shaderParms[8];
+		Image const* texMRAO;
+		Image const* texDiffuse;
+		Image const* texNormal;
+		Image const* texEmissive;
+		Image const* texAO;	// if exists then same as texMRAO
+		glm::vec4 kDiffuse;
+		float kEmissive;
+		float kMetallic;
+		float kRoughness;
+		float kAmbientOcclusion;
+		float alphaCutoff;
+		bool doubleSided;
+		eCoverage coverage;
 	};
 }
